3aa197d000ad90d1270ad0984f811b9292636aff
sayon-islam-23 <68483305+sayon-islam-23@users.noreply.github.com>
2021-05-08 19:47:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+const texts = document.querySelector(".texts");
+
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+let p = document.createElement("p");
+
+recognition.addEventListener("result", (e) => {
+  texts.appendChild(p);
+  const text = Array.from(e.results)
+    .map((result) => result[0])
+    .map((result) => result.transcript)
+    .join("");
+
+  p.innerText = text;
+  if (e.results[0].isFinal) {
+    if (text.includes("how are you")) {
+      p = document.createElement("p");
+      p.classList.add("replay");
+      p.innerText = "I am fine";
+      texts.appendChild(p);
+    }
+    if (
+      text.includes("what's your name") ||
+      text.includes("what is your name")
+    ) {
+      p = document.createElement("p");
+      p.classList.add("replay");
+      p.innerText = "My Name is Siri-jr";
+      texts.appendChild(p);
+    }
+    if (text.includes("open YouTube")) {
+      p = document.createElement("p");
+      p.classList.add("replay");
+      p.innerText = "opening youtube ";
+      texts.appendChild(p);
+      console.log("opening youtube");
+      window.open("https://www.youtube.com/channel/UCdxaLo9ALJgXgOUDURRPGiQ");
+    }
+    if (text.includes("open Google")) {
+        p = document.createElement("p");
+        p.classList.add("replay");
+        p.innerText = "opening Google ";
+        texts.appendChild(p);
+        console.log("opening Google");
+        window.open("https://google.co.in");
+      }
+    p = document.createElement("p");
+  }
+});
+
+recognition.addEventListener("end", () => {
+  recognition.start();
+});
+
+recognition.start();
